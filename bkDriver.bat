754a621fc669685cee24bd868188c13cf37b33f7
Daniel P. Castro <daniel.pcastro11@gmail.com>
2022-06-23 13:29:15 -0300

    Adicionando script de automacao

---
+++ bkDriver.bat
@@ -0,0 +1,6 @@
+REM Copia o Cofre para o DriveVault
+robocopy "N:\Vault" "N:\DriveVault\Vault" /E /COPYALL /Z /R:5 /W:5 /XO /PURGE /LOG:S:\LogBackup\bkpDrive-%date:/=%.log
+
+REM Executa o Google Driver
+pause
+REM exit
---
+++ bkMega.bat
@@ -0,0 +1,4 @@
+REM Copia o Cofre para o MegaVault
+robocopy "N:\Vault" "N:\MegaVault\Vault" /E /COPYALL /Z /R:5 /W:5 /XO /PURGE /LOG:S:\LogBackup\bkpMega-%date:/=%.log
+
+REM exit
